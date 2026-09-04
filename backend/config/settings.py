8af1411940dbe48faa41bda3ab98c95baa7b5dfd
zurriyatt <hzurriyat@gmail.com>
2026-09-04 17:13:09 +0500

    updatingpakage

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,16 +15,17 @@ from dotenv import load_dotenv
 import os
 import dj_database_url
 
+# Build paths
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+# Load environment from backend/.env and override any system variables
+load_dotenv(BASE_DIR / '.env', override=True)
+
 print("DB_URL is:", os.environ.get('DB_URL'))
+
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DB_URL'))
 }
-# Now use os.environ.get('DATABASE_URL') or dj_database_url
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
